47561a72072f807c622936c747f917b65af713d6
Jigyasa05 <jiya86.j@gail.com>
2024-01-10 14:53:06 +0530

    ...

--- frontend/src/components/AddSales.jsx
+++ frontend/src/components/AddSales.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import {useFormik} from "formik"
 
 
+
+   
 const AddSales = () => {
 
+    const signupForm = useFormik({
+        initialValues: {
+            Shopname : '',
+            Category : '',
+            Saledescription : '',
+            Offer : '', 
+            Address : '',
+            Startdata : '',
+            Enddata : '',
+            Contact : '',
+            Image : '',
+        },
+        onSubmit: async (values, { resetForm }) => {
+          // alert(JSON.stringify(values));
+          console.log(values);
     
+          // send request to backend/REST API
+          const response = await fetch('http://localhost:5000/users/add', {
+            method: 'POST',
+            body: JSON.stringify(values),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
+    
+          console.log(response.status);
+          console.log(response.statusText);
+    
+          if (response.status === 200) {
+            enqueueSnackbar('registered Successfully', { variant: 'success' });
+          } else {
+            enqueueSnackbar('Something went wrong', { variant: 'error' });
+          }
+    
+          // resetForm();
+          // toast.success('Form Submitted Successfully');
+    
+        },
+        
+      });
+
+
     return (
         <div>
             <div className="container">
@@ -12,11 +56,18 @@ const AddSales = () => {
                     <form action="">
                         <div>
                         <label htmlFor="" className="form-label">Shop Name</label>
-                        <input type="text" className="form-cotrol" />
+                        <input type="text"
+                         className="form-cotrol"
+                         id="Shopname"
+                              value={AddSalesForm.values.Shopname}
+                              onChange={AddSalesForm.handleChange} />
                         </div>
                         <div>
                         <label htmlFor="" className="form-label">Categories</label>
-                        <input type="text" className="form-cotrol" />
+                        <input type="text" className="form-cotrol"
+                        id="Categories"
+                        value={AddSalesForm.values.Categories}
+                        onChange={AddSalesForm.handleChange} />
                         </div>
                         <div>
                         <label htmlFor="" className="form-label">Salesdescription</label>
@@ -24,24 +75,42 @@ const AddSales = () => {
                         </div>
                         <div>
                         <label htmlFor="" className="form-label">Offer</label>
-                        <input type="text" className="form-cotrol" />
+                        <input type="text" className="form-cotrol"
+                        id="Offer"
+                        value={AddSalesForm.values.Offer}
+                        onChange={AddSalesForm.handleChange} />
                         </div>
                         <div>
-                        <label htmlFor="" className="form-label">address</label>
-                        <input type="text" className="form-cotrol" />
+                        <label htmlFor="" className="form-label">Address</label>
+                        <input type="text" className="form-cotrol" 
+                        id="Address"
+                        value={AddSalesForm.values.Address}
+                        onChange={AddSalesForm.handleChange}/>
                         </div>
                         <div>
-                        <label htmlFor="" className="form-label">Shp Name</label>
-                        <input type="text" className="form-cotrol" />
+                        <label htmlFor="" className="form-label">Start Date</label>
+                        <input type="text" className="form-cotrol"
+                        id="Startdata"
+                        value={AddSalesForm.values.Startdata}
+                        onChange={AddSales.handleChange} />
                         </div>
                         <div>
-                        <label htmlFor="" className="form-label">Shp Name</label>
-                        <input type="text" className="form-cotrol" />
+                        <label htmlFor="" className="form-label">End Data</label>
+                        <input type="text" className="form-cotrol" 
+                        id="Enddata"
+                        value={AddSalesForm.values.Enddata}
+                        onChange={AddSalesForm.handleChange}/>
                         </div>
                         <div>
-                        <label htmlFor="" className="form-label">Shp Name</label>
-                        <input type="text" className="form-cotrol" />
+                        <label htmlFor="" className="form-label">Contact</label>
+                        <input type="text" className="form-cotrol"
+                        id="Contact"
+                        value={AddSalesForm.values.Contact}
+                        onChange={AddSalesForm.handleChange} />
                         </div>
+                        <button type="submit" className="btn btn-primary btn-lg">
+                            Submit
+                          </button> 
                     </form>
                 </div>
 
@@ -53,4 +122,4 @@ const AddSales = () => {
     )
 }
 
-export default AddSales
+export default AddSales;
